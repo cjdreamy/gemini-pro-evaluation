2561ad238b3b7ee0cbc1d5094af7cf4e5caa42c2
cjdreamy <josephalocye402@gmail.com>
2026-03-18 19:31:57 +0300

    added ui

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 import { GoogleGenAI } from "@google/genai";
 import dotenv from "dotenv";
+import express from "express";
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));  
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
 dotenv.config();
 
@@ -8,18 +16,30 @@ const ai = new GoogleGenAI({
   apiKey: `${process.env.GEMINI_API_KEY}` 
 });
 
-async function run() {
-  try {
-    // NEW SYNTAX: Use ai.models.generateContent
+app.post("/chat", async (req, res) => {
+    const prompt = req.body.prompt;
     const result = await ai.models.generateContent({
-      model: "gemini-3-flash-preview", 
-      contents: [{ role: "user", parts: [{ text: "Hi, which version are you?" }] }]
+        model: "gemini-3-flash-preview",
+        contents: [{ role: "user", parts: [{ text: prompt }] }]
     });
+    res.render("index", { response: result.text });
+});
 
-    console.log(result.text); 
-  } catch (error) {
-    console.error("Error calling Gemini:", error);
-  }
-}
+// async function run() {
+//   try {
+//     // NEW SYNTAX: Use ai.models.generateContent
+//     const result = await ai.models.generateContent({
+//       model: "gemini-3-flash-preview", 
+//       contents: [{ role: "user", parts: [{ text: "Hi, which version are you?" }] }]
+//     });
 
-run();
+//     console.log(result.text); 
+//   } catch (error) {
+//     console.error("Error calling Gemini:", error);
+//   }
+// }
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
+// run();
